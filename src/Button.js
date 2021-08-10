abdf38c16a3abf65cba0ddb51044a21ea14c0cb3
Roy Kashi <royk@Orpak.com>
2021-08-10 15:37:45 +0300

    fix

--- src/Button.js
+++ src/Button.js
@@ -10,9 +10,8 @@ const Button = () => {
   const print = () => {
     console.log('Print Clicked!');
     fetch('receipt.txt').then(response => response.text()).then(htmlReceipt => {
-      //TODO: add the communication to the native app
       console.log(htmlReceipt);
-      //TODO: add the communication to the native app
+      JSBridge.showMessageInNative(valueReceived);
     });
   };
   return (<button style={style} onClick={print}>App Button</button>)
